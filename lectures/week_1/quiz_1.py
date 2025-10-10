9dfff9412ce86412ca66e12444ce43d4bddb272a
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-10-10 15:17:12 +0300

    quiz-1'in fotografi eklendi.

--- lectures/week_1/quiz_1.py
+++ lectures/week_1/quiz_1.py
@@ -1,11 +1,12 @@
 # e üzeri x değerini yaklaşık olarak hesapla. (Nasıl hesaplanacağına dair formül verilir.)
+# quiz-1-question.jpg den sorunun tamamına ve çözümüne ulaşabilirsiniz.
 
 result = 0
 x = int(input("Number : "))
 
 for n in range(0, 25):
     fac = 1
-    for j in range(1, n+1):s
+    for j in range(1, n+1):
         fac *= j
         pass
     result += (x**n)/ fac
