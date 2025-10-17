1f95b2bd533c9de23cee380b2d82625a9588f430
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-10-18 00:25:26 +0300

    Yorum satırları eklendi.

--- lectures/week_3/lab3_task_2.py
+++ lectures/week_3/lab3_task_2.py
@@ -9,18 +9,14 @@ the given row (i-th) and column (j-th) index using the following rules.
 • f(i, j=i) = 2 for i > 1  
 • j <= i  
 
+NOT :
+
+Mantık şu hücremizdeki değer. bir birim solundaki ve sol üst
+çaprazındaki elemanların toplamı.
+Bu mantığa sadık kalarak aradığımız hücreye gelene kadar tüm
+boşlukları dolduracağız.
+
 """
-
-def column_2(row):
-    """column_2
-
-    Args:
-        row (int): Row valule where column is 2
-
-    Returns:
-        int : cell value
-    """
-    return (row - 2 - 1) * 3 + 2
 
 def functionT2(row, column):
     
@@ -30,7 +26,7 @@ def functionT2(row, column):
     ####################################
     
     if  not (0 < row    < 100) or \
-        not (0 < column < 100) and \
+        not (0 < column < 100) or \
         not (row >= column):
         return 0
     
@@ -49,23 +45,32 @@ def functionT2(row, column):
     previous_col = [] 
     
     for j in range(2, column + 1):
+        
+        # Mevcut kolon
         col = []
+        # ilk deger 2
         val = 2
+        
         for i in range(j+1, row + 1):
             
-            if j == 2:   # ilk kolondaysak
+            if j == 2:   
+                # 2. kolondaysak  3 ekle
+                # cunku 1. kolonda sadece 3 var
                 val = 3 + val
             else:                
                 val =  previous_col[i-(j+1)] + val
 
+            # elde edilen degeri col da sakla
             col.append(val)     
-            print(col)
                        
-            if i == row and j == column:
-                return val        
+            # if i == row and j == column:
+            #     return val        
         pass
         previous_col = col
     pass
+    # Son eleman aradığımız hücredeki eleman3
+    return previous_col[-1]
+
 
 
 
